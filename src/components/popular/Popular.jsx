bcf1a52218cb9e95eb140bfc8fff750e47667433
6005168766pd <officialprem707@gmail.com>
2025-07-28 16:50:07 +0530

    Update Popular.jsx

--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -4,7 +4,7 @@ import Item from "../Item/Item";
 const Popular = () => {
   const [popularproducts, setpopularproducts] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:4000/popularinwomen")
+    fetch("https://ecommerce-app-backend-snc9.onrender.com/popularinwomen")
       .then((res) => res.json())
       .then((data) => setpopularproducts(data));
   }, []);
